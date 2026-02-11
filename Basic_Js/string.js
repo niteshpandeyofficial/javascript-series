e45355994e16c0742bd8b000972aee1fd0fcb7d3
Nitesh Pandey <niteshpandeyofficial@gmail.com>
2026-02-11 16:09:02 +0000

    updated the code

--- Basic_Js/string.js
+++ Basic_Js/string.js
@@ -1,7 +1,24 @@
-let name = "nitesh"
+let name = "nitesh-official"
 let surname = "pandey"
 
 console.log(name +" SarveshKumar" +surname) //not recomended way to concatenate or write this type of code
 
 console.log(`my name is ${name} and surname is ${surname}`)
+
+console.log(surname.toUpperCase());
+console.log(surname.charAt(0));
+console.log(surname.indexOf('n'))
+
+
+console.log(name.substring(0,4))
+
+const newString="Python"
+console.log(newString.slice(-2))
+
+console.log(" nitesh ".trim())
+
+const urlId="https://cracklogic.com"
+console.log(urlId.replace('https://cracklogic.com','technocutter'))
+
+
 
